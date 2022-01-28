eb39f78d4dd0ae54373717741bb7f92fcab598e1
Zhanghaotian1 <74804291+Zhanghaotian1@users.noreply.github.com>
2022-01-28 20:44:26 +0800

    Delete SDE_CG_C directory

--- SDE_CG_C/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from .loss_fn import loss_fn
-from .scorenet import ScoreNet
-from .SDE_builder import marginal_prob_std, diffusion_coeff
-__all__ = ['loss_fn','ScoreNet','marginal_prob_std','diffusion_coeff']
