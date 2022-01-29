ddc393bf6e5bd2e55ad1653e1fa0883c297498f7
Zhanghaotian1 <74804291+Zhanghaotian1@users.noreply.github.com>
2022-01-29 13:10:43 +0800

    Delete SDE_CG directory

--- SDE_CG/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .loss_fn import loss_fn_discretized
-from .scorenrt import ScoreNet_discretized
-__all__ = ['loss_fn_discretized','ScoreNet_discretized']
--- SDE_CG/model/loss_fn.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def loss_fn_discretized(score, target, sigmas):
-    if score.shape[1] == 1 :
-        score = score.view(-1)
-        target = target.view(-1)
-        sigmas = sigmas.view(-1)
-    loss = 0.5 *((score - target) **2 ) * sigmas
-    return torch.mean(loss)
