5e561b8f8804c277b45ac163fcdfb59ac944f455
Zhanghaotian1 <74804291+Zhanghaotian1@users.noreply.github.com>
2022-02-08 21:05:18 +0800

    Update __init__.py

--- SDE_CG/model/__init__.py
+++ SDE_CG/model/__init__.py
@@ -1,3 +1,3 @@
 from .loss_fn import loss_fn_discretized
-from .scorenrt import ScoreNet_discretized
+from .scorenet import ScoreNet_discretized
 __all__ = ['loss_fn_discretized','ScoreNet_discretized']
