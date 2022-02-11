266b659a3893b8eefea364056357b863860e2d7f
Zhanghaotian1 <74804291+Zhanghaotian1@users.noreply.github.com>
2022-02-11 13:03:04 +0800

    Update scorenet.py

--- SDE_CG_C/model/scorenet.py
+++ SDE_CG_C/model/scorenet.py
@@ -26,6 +26,27 @@ class ScoreNet(torch.nn.Module):
         self.dense1 = nn.Linear(hidden_dim, 1)
         self.marginal_prob_std = marginal_prob_std
         self.device = device
+        
+    @torch.no_grad()
+    def get_score(self,data,d,t):
+        t_embedding = self.t_embed(t)
+        t_embedding = self.dense1(t_embedding)
+        node2graph = data.batch
+        edge2graph = node2graph[data.edge_index[0]]
+        atom_attr = self.atom_emb(data.node_type)  # (atom_dim, hidden_dim)
+        bond_attr = self.bond_emb(data.edge_type)  # (edge_dim, hidden_dim)
+        d_emb = self.input_mlp(d)  # (edge_dim, hidden_dim) =[58234, 256]
+        d_emb += t_embedding[edge2graph]
+        bond_attr = d_emb * bond_attr  # (edge_dim, hidden_dim) =[58234, 256]
+        output = self.model(data, atom_attr, bond_attr)
+        h_row, h_col = output["node_feature"][data.edge_index[0]], output["node_feature"][data.edge_index[1]]
+        distance_feature = torch.cat([h_row * h_col, bond_attr], dim=-1)  # (edge_dim, 2*hidden_dim) =[58234, 512]
+        scores = self.output_mlp(distance_feature)  # (edge_dim, 1) = [58234, 1]
+        scores = scores.view(-1)
+        scores = scores / self.marginal_prob_std(t[edge2graph]).to(self.device)
+        return scores
+    
+    
     def forward(self, data, t):
         '''
         Score_Net function, which is constructed by two MLP blocks, two embedding layers for atom and bond attribution
